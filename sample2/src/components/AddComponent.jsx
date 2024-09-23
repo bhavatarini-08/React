070e1afb3e51bfa303f121d980f280ff3e098f36
bhavatarini-08 <bhavatariniselvan@gmail.com>
2024-09-23 19:00:42 +0530

    Updated PojectCard.jsx

--- sample2/src/components/AddComponent.jsx
+++ sample2/src/components/AddComponent.jsx
@@ -37,13 +37,16 @@ const AddComponent = () => {
                 Add
                 <Plus className='h-6 w-6 ml-2' />
             </button> */}
-            <button 
-  onClick={() => setvisible(true)} 
-  className='flex justify-center items-center text-white bg-purple-500 hover:bg-purple-600 px-4 py-2 rounded-md shadow-lg transform hover:scale-105 transition-transform duration-300 mt-4'
->
-  Add
-  <Plus className='h-6 w-6 ml-2' />
-</button>
+          <div className="flex justify-end">
+  <button 
+    onClick={() => setvisible(true)} 
+    className='flex justify-center items-center text-white bg-purple-500 hover:bg-purple-600 px-4 py-2 rounded-md shadow-lg transform hover:scale-105 transition-transform duration-300 mt-4 float-right'
+  >
+    Add Your Projects
+    <Plus className='h-6 w-6 ml-2' />
+  </button>
+</div>
+
 
             {
                 visible && (
